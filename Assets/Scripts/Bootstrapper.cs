4a9e67a19ba9b84e1275be6d7c61fbf4e5f1f2b8
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-12-08 20:16:17 +0000

    Removed unused code

--- Assets/Scripts/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*/using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -31,4 +31,4 @@ public class Bootstrapper
 
     }
 
-}
+}*/
