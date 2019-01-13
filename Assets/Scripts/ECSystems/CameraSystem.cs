addbda7927f4fe6865d5876e0fe94e65982ee572
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-01-13 11:33:04 +0000

    removed chunk iteration

--- Assets/Scripts/ECSystems/CameraSystem.cs
+++ Assets/Scripts/ECSystems/CameraSystem.cs
@@ -15,14 +15,8 @@ public class CameraSystem : ComponentSystem
 
     public static Entity playerEntity;
 
-    EntityArchetypeQuery query;
-
-    ArchetypeChunkEntityType                entityType;
-    ArchetypeChunkComponentType<Position>   positionType;
-
     Camera camera;
     float cameraSwivelSpeed = 1;
-    //float3 currentOffset = new float3(0, 15, 10);
     float3 currentOffset = new float3(0, 10, 10);
 
     protected override void OnCreateManager()
@@ -30,93 +24,63 @@ public class CameraSystem : ComponentSystem
         entityManager = World.Active.GetOrCreateManager<EntityManager>();
         cubeSize = TerrainSettings.cubeSize;
 
-        query = new EntityArchetypeQuery
-        {
-            Any     = Array.Empty<ComponentType>(),
-            None    = Array.Empty<ComponentType>(),
-            All     = new ComponentType[] { typeof(Tags.PlayerEntity) }
-        };
-
-        //DEBUG
         camera = GameObject.FindObjectOfType<Camera>();
     }
 
     protected override void OnUpdate()
     {
-        entityType      = GetArchetypeChunkEntityType();
-        positionType    = GetArchetypeChunkComponentType<Position>();
-
-        NativeArray<ArchetypeChunk> chunks;
-        chunks = entityManager.CreateArchetypeChunkArray(
-            query,
-            Allocator.TempJob
-        );
-
-        if(chunks.Length == 0) chunks.Dispose();
-        else MoveCamera(chunks);
+       MoveCamera();
     }
 
-    void MoveCamera(NativeArray<ArchetypeChunk> chunks)
+    void MoveCamera()
     {
-        for(int c = 0; c < chunks.Length; c++)
+        float3 playerPosition   = entityManager.GetComponentData<Position>(playerEntity).Value;
+
+        float3 oldPosition      = camera.transform.position;
+        Quaternion oldRotation  = camera.transform.rotation;
+
+        //  Rotate around player y axis
+        bool Q = Input.GetKey(KeyCode.Q);
+        bool E = Input.GetKey(KeyCode.E);
+        Quaternion cameraSwivel = Quaternion.identity;
+        if( !(Q && E) )
         {
-            ArchetypeChunk chunk = chunks[c];
-
-            NativeArray<Entity> entities    = chunk.GetNativeArray(entityType);
-            NativeArray<Position> positions = chunk.GetNativeArray(positionType);
-            
-            for(int e = 0; e < entities.Length; e++)
-            {
-                float3 playerPosition   = positions[e].Value;
-
-                float3 oldPosition      = camera.transform.position;
-                Quaternion oldRotation  = camera.transform.rotation;
-
-                //  Rotate around player y axis
-                bool Q = Input.GetKey(KeyCode.Q);
-                bool E = Input.GetKey(KeyCode.E);
-                Quaternion cameraSwivel = Quaternion.identity;
-                if( !(Q && E) )
-                {
-                    if (Q) cameraSwivel     = Quaternion.Euler(new float3(0, -cameraSwivelSpeed, 0));
-                    else if(E) cameraSwivel = Quaternion.Euler(new float3(0, cameraSwivelSpeed, 0));
-                }
-                float3 rotateOffset = (float3)oldPosition - Util.RotateAroundCenter(cameraSwivel, oldPosition, playerPosition);
-
-                //  Zoom with mouse wheel
-                float3 zoomOffset = Input.GetAxis("Mouse ScrollWheel") * -currentOffset;
-
-                //  Apply position changes
-                currentOffset += rotateOffset;
-
-                //  Clamp zoom 
-                float3 withZoom = currentOffset + zoomOffset;
-                float magnitude = math.sqrt(math.pow(withZoom.x, 2) + math.pow(withZoom.y, 2) + math.pow(withZoom.z, 2));
-                if(magnitude > 10 && magnitude < 40)
-                {
-                    //  x & z smoothed for zoom because y is smoothed for everything later
-                    //  This prevents jumpy camera movement when zooming
-                    currentOffset = new float3(
-                        math.lerp(currentOffset.x, withZoom.x, 0.1f),
-                        withZoom.y,
-                        math.lerp(currentOffset.z, withZoom.z, 0.1f)
-                    );
-                }
-
-                //  New position and rotation without any smoothing
-                float3 newPosition      = playerPosition + currentOffset;
-                Quaternion newRotation  = Quaternion.LookRotation(playerPosition - (float3)oldPosition, Vector3.up);
-
-                //  Smooth y for everything
-                //  Movement depends on camera angle and lerping x & z causes camera
-                //  to turn when moving, so we only smooth y for movement
-                float yLerp = math.lerp(oldPosition.y, newPosition.y, 0.1f);
-
-                //  Apply new position and rotation softly
-                camera.transform.position = new float3(newPosition.x, yLerp, newPosition.z);
-                camera.transform.rotation = Quaternion.Lerp(oldRotation, newRotation, 0.1f);
-            }
+            if (Q) cameraSwivel     = Quaternion.Euler(new float3(0, -cameraSwivelSpeed, 0));
+            else if(E) cameraSwivel = Quaternion.Euler(new float3(0, cameraSwivelSpeed, 0));
         }
-        chunks.Dispose();
+        float3 rotateOffset = (float3)oldPosition - Util.RotateAroundCenter(cameraSwivel, oldPosition, playerPosition);
+
+        //  Zoom with mouse wheel
+        float3 zoomOffset = Input.GetAxis("Mouse ScrollWheel") * -currentOffset;
+
+        //  Apply position changes
+        currentOffset += rotateOffset;
+
+        //  Clamp zoom 
+        float3 withZoom = currentOffset + zoomOffset;
+        float magnitude = math.sqrt(math.pow(withZoom.x, 2) + math.pow(withZoom.y, 2) + math.pow(withZoom.z, 2));
+        if(magnitude > 10 && magnitude < 40)
+        {
+            //  x & z smoothed for zoom because y is smoothed for everything later
+            //  This prevents jumpy camera movement when zooming
+            currentOffset = new float3(
+                math.lerp(currentOffset.x, withZoom.x, 0.1f),
+                withZoom.y,
+                math.lerp(currentOffset.z, withZoom.z, 0.1f)
+            );
+        }
+
+        //  New position and rotation without any smoothing
+        float3 newPosition      = playerPosition + currentOffset;
+        Quaternion newRotation  = Quaternion.LookRotation(playerPosition - (float3)oldPosition, Vector3.up);
+
+        //  Smooth y for everything
+        //  Movement depends on camera angle and lerping x & z causes camera
+        //  to turn when moving, so we only smooth y for movement
+        float yLerp = math.lerp(oldPosition.y, newPosition.y, 0.1f);
+
+        //  Apply new position and rotation softly
+        camera.transform.position = new float3(newPosition.x, yLerp, newPosition.z);
+        camera.transform.rotation = Quaternion.Lerp(oldRotation, newRotation, 0.1f);
     }
 }
