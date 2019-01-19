1d827ed7cd6a831ba47c1583208538a9d5c2cb0a
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-01-19 12:49:37 +0000

    Added new system

---
+++ Assets/Scripts/ECSystems/MapBufferChangeSystem.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using MyComponents;
+
+//	Generate 3D block data from 2D terrain data
+[UpdateAfter(typeof(MapOuterBufferSystem))]
+public class MapBufferChangeSystem : ComponentSystem
+{
+	EntityManager entityManager;
+
+	int cubeSize;
+
+	ArchetypeChunkEntityType 				entityType;
+	ArchetypeChunkComponentType<MapSquare>	mapSquareType;
+	ArchetypeChunkBufferType<Block> 		blocksType;
+	ArchetypeChunkBufferType<Topology> 		heightmapType;
+	
+
+	EntityArchetypeQuery mapSquareQuery;
+
+	protected override void OnCreateManager()
+	{
+		entityManager = World.Active.GetOrCreateManager<EntityManager>();
+		cubeSize = TerrainSettings.cubeSize;
+
+		mapSquareQuery = new EntityArchetypeQuery
+		{
+			Any 	= Array.Empty<ComponentType>(),
+			None  	= new ComponentType[] { typeof(Tags.EdgeBuffer), typeof(Tags.OuterBuffer) },
+			All  	= new ComponentType[] { typeof(MapSquare), typeof(BufferChange) }
+		};
+	}
+
+	protected override void OnUpdate()
+	{
+		entityType 		= GetArchetypeChunkEntityType();
+		mapSquareType	= GetArchetypeChunkComponentType<MapSquare>();
+
+		blocksType 		= GetArchetypeChunkBufferType<Block>();
+        heightmapType = GetArchetypeChunkBufferType<Topology>();
+
+		NativeArray<ArchetypeChunk> chunks = entityManager.CreateArchetypeChunkArray(
+			mapSquareQuery,
+			Allocator.TempJob
+			);
+
+		if(chunks.Length == 0) chunks.Dispose();
+		else GenerateCubes(chunks);
+	}
+
+	void GenerateCubes(NativeArray<ArchetypeChunk> chunks)
+	{
+		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.Temp);
+
+		for(int c = 0; c < chunks.Length; c++)
+		{
+			ArchetypeChunk chunk = chunks[c];
+
+			NativeArray<Entity> entities 				= chunk.GetNativeArray(entityType);
+			NativeArray<MapSquare> mapSquares			= chunk.GetNativeArray(mapSquareType);
+			BufferAccessor<Block> blockAccessor 		= chunk.GetBufferAccessor(blocksType);
+            BufferAccessor<Topology> heightmapAccessor 	= chunk.GetBufferAccessor(heightmapType);
+			
+			for(int e = 0; e < entities.Length; e++)
+			{
+				Entity entity 						= entities[e];
+				DynamicBuffer<Block> blockBuffer 	= blockAccessor[e];
+                DynamicBuffer<Topology> heightmap		= heightmapAccessor[e];
+
+				MapSquare mapSquare = entityManager.GetComponentData<MapSquare>(entity);
+
+				//	Resize buffer to size of (blocks in a cube) * (number of cubes)
+				blockBuffer.ResizeUninitialized(mapSquare.blockGenerationArrayLength);
+
+				//	Set slopes next
+				commandBuffer.RemoveComponent<Tags.GenerateBlocks>(entity);
+			}
+		}
+		
+		commandBuffer.Playback(entityManager);
+		commandBuffer.Dispose();
+
+		chunks.Dispose();
+	}
+
+}
