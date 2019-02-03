68f2b015635c52af06e440255ebff02c8c8835b6
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-02-03 18:10:43 +0000

    Cursor cube

--- Assets/Scripts/ECSystems/PlayerInputSystem.cs
+++ Assets/Scripts/ECSystems/PlayerInputSystem.cs
@@ -17,6 +17,24 @@ public class PlayerInputSystem : ComponentSystem
 
     public static Entity playerEntity;
     Camera camera;
+    Entity cursorCube;
+
+    struct VoxelRayHit
+    {
+        readonly public int blockIndex;
+        readonly public Entity blockOwner, faceBlockOwner;
+        readonly public Block hitBlock, faceHitBlock;
+        readonly public float3 worldPosition;
+        public VoxelRayHit(int blockIndex, Entity blockOwner, Block hitBlock, Block faceHitBlock, Entity faceBlockOwner, float3 worldPosition)
+        {
+            this.blockIndex         = blockIndex;
+            this.blockOwner         = blockOwner;
+            this.hitBlock           = hitBlock;
+            this.faceHitBlock       = faceHitBlock;
+            this.faceBlockOwner     = faceBlockOwner;
+            this.worldPosition      = worldPosition;
+        }
+    }
 
     protected override void OnCreateManager()
     {
@@ -24,8 +42,23 @@ public class PlayerInputSystem : ComponentSystem
         squareWidth = TerrainSettings.mapSquareWidth;
 
         camera = GameObject.FindObjectOfType<Camera>();
+
+        CreateCursorCube();
     }
 
+    void CreateCursorCube()
+    {
+         EntityArchetype cursorCubeArchetype = entityManager.CreateArchetype(
+            ComponentType.Create<Position>(),
+            ComponentType.Create<RenderMeshComponent>()
+		);
+
+        cursorCube = entityManager.CreateEntity(cursorCubeArchetype);
+        CursorCubeMesh(cursorCube);
+    }
+
+    Block previousBlock;
+    Entity previousBlockOwner;
     protected override void OnUpdate()
     {
         if(Time.fixedTime < 0.5) return;
@@ -34,16 +67,22 @@ public class PlayerInputSystem : ComponentSystem
 
         VoxelRayHit hit;
 
-        //bool targetingBlock = VoxelRay(camera.ScreenPointToRay(Input.mousePosition), out hit);
+        bool targetingBlock = VoxelRay(camera.ScreenPointToRay(Input.mousePosition), out hit);
+
+        if(targetingBlock)
+        {
+            Position newPosition = new Position{ Value = hit.worldPosition };
+            entityManager.SetComponentData<Position>(cursorCube, newPosition);
+        }
 
         if(Input.GetButtonDown("Fire1")/* && targetingBlock */)
         {
-            if(VoxelRay(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            if(targetingBlock)
                 ChangeBlock(0, hit.hitBlock, hit.blockOwner);
         }
         else if(Input.GetButtonDown("Fire2")/* && targetingBlock */)
         {
-            if(VoxelRay(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            if(targetingBlock)
                 ChangeBlock(1, hit.faceHitBlock, hit.faceBlockOwner);
         }
     }
@@ -72,21 +111,13 @@ public class PlayerInputSystem : ComponentSystem
         block.type = type;
         MapUpdateSystem.GetOrCreatePendingChangeBuffer(owner, entityManager).Add(new PendingChange { block = block });
     }
-
-    struct VoxelRayHit
+    void DebugBlock(int debug, Block block, Entity owner)
     {
-        readonly public int blockIndex;
-        readonly public Entity blockOwner, faceBlockOwner;
-        readonly public Block hitBlock, faceHitBlock;
-        public VoxelRayHit(int blockIndex, Entity blockOwner, Block hitBlock, Block faceHitBlock, Entity faceBlockOwner)
-        {
-            this.blockIndex         = blockIndex;
-            this.blockOwner         = blockOwner;
-            this.hitBlock           = hitBlock;
-            this.faceHitBlock       = faceHitBlock;
-            this.faceBlockOwner     = faceBlockOwner;
-        }
+        block.debug = debug;
+        MapUpdateSystem.GetOrCreatePendingChangeBuffer(owner, entityManager).Add(new PendingChange { block = block });
     }
+
+    
 
     //  Return list of voxel positions hit by ray from eye to dir
     bool VoxelRay(Ray ray, out VoxelRayHit hit)
@@ -210,7 +241,7 @@ public class PlayerInputSystem : ComponentSystem
 
                 int previousIndex = Util.BlockIndex(previousVoxel, previousMapSquare, squareWidth);
 
-                hit = new VoxelRayHit(index, entity, blocks[index], blocks[previousIndex], previousEntity);
+                hit = new VoxelRayHit(index, entity, blocks[index], blocks[previousIndex], previousEntity, mapSquare.position + blocks[index].localPosition);
                 return true;
             }
 
@@ -220,6 +251,78 @@ public class PlayerInputSystem : ComponentSystem
         }
         throw new Exception("Ray traversed 1000 voxels without finding anything");
     }
+
+    //	Vertices for normal cube
+	void CursorCubeMesh(Entity cubeEntity)
+	{	
+        CubeVertices baseVerts = new CubeVertices(true);
+
+        Vector3[] vertices = new Vector3[24];
+
+        int[] triangles = new int[36];
+
+        vertices[0]  = baseVerts[5] * 1.2f;
+        vertices[1]  = baseVerts[6] * 1.2f;
+        vertices[2]  = baseVerts[2] * 1.2f;
+        vertices[3]  = baseVerts[1] * 1.2f;
+        vertices[4]  = baseVerts[7] * 1.2f;
+        vertices[5]  = baseVerts[4] * 1.2f;
+        vertices[6]  = baseVerts[0] * 1.2f;
+        vertices[7]  = baseVerts[3] * 1.2f;
+        vertices[8]  = baseVerts[4] * 1.2f;
+        vertices[9]  = baseVerts[5] * 1.2f;
+        vertices[10] = baseVerts[1] * 1.2f;
+        vertices[11] = baseVerts[0] * 1.2f;
+        vertices[12] = baseVerts[6] * 1.2f;
+        vertices[13] = baseVerts[7] * 1.2f;
+        vertices[14] = baseVerts[3] * 1.2f;
+        vertices[15] = baseVerts[2] * 1.2f;
+        vertices[16] = baseVerts[7] * 1.2f;
+        vertices[17] = baseVerts[6] * 1.2f;
+        vertices[18] = baseVerts[5] * 1.2f;
+        vertices[19] = baseVerts[4] * 1.2f;
+        vertices[20] = baseVerts[0] * 1.2f;
+        vertices[21] = baseVerts[1] * 1.2f;
+        vertices[22] = baseVerts[2] * 1.2f;
+        vertices[23] = baseVerts[3] * 1.2f;
+
+        int index = 0;
+        int vertIndex = 0;
+        for(int i = 0; i < 6; i++)
+        {
+            triangles[index+0] = 3 + vertIndex; 
+            triangles[index+1] = 1 + vertIndex; 
+            triangles[index+2] = 0 + vertIndex; 
+            triangles[index+3] = 3 + vertIndex; 
+            triangles[index+4] = 2 + vertIndex; 
+            triangles[index+5] = 1 + vertIndex;
+
+            index += 6;
+            vertIndex += 4;
+        }
+
+        Color[] colors = new Color[24];
+        
+        for(int i = 0; i < 24; i++)
+        {
+            colors[i] = new Color(1, 1, 1, 0.2f);
+        }
+
+        Mesh mesh 		= new Mesh();
+		mesh.vertices 	= vertices;
+        mesh.colors     = colors; 
+		mesh.SetTriangles(triangles, 0);
+
+		mesh.RecalculateNormals();
+
+        RenderMesh renderer = new RenderMesh();
+		renderer.mesh = mesh;
+		renderer.material = MapMeshSystem.material;
+
+		entityManager.AddSharedComponentData(cubeEntity, renderer);
+	}
+
+
 
     bool GetBlockOwner(float3 currentMapSquarePostion, out Entity owner)
     {
