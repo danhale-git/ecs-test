1dfdf3b2a60bfd76cd1a8f9c2f3380a4ef3dc558
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-02-09 06:40:44 +0000

    starting noise improvements

--- Assets/Scripts/CliffTerrainGenerator.cs
+++ Assets/Scripts/CliffTerrainGenerator.cs
@@ -28,106 +28,30 @@ public class CliffTerrainGenerator
         cliffBleed = 0.025f;
     }
 
-    Topology GetHeight(float noise)
-    {
-        int height = 0;
-        TerrainTypes type = 0;
-
-        float increment = 1.0f / levelCount;
-
-        float depth = cliffDepth;
-        float bleed = cliffBleed;
-
-        for(int l = 1; l < levelCount; l++)
-        {
-            int topHeight = levelHeight * l;
-            int bottomHeight = topHeight - levelHeight;
-
-            float start = (increment * l) - (depth / 2);
-            float end = (increment * l) + (depth / 2);
-
-            float nextStart = (increment * (l+1)) - (depth / 2);
-            float prevEnd = (increment * (l-1)) + (depth / 2);
-
-            if(noise > start - bleed && noise < end + bleed)
-            {
-                type = TerrainTypes.CLIFF;
-                if( noise > start && noise < end)
-                {
-                    float interp = Mathf.InverseLerp(start, end, noise);
-                    height = (int)math.lerp(bottomHeight, topHeight, interp);
-                }
-                else if(noise >= end) height = topHeight;
-                else height = bottomHeight;
-                break;
-            }
-            if(noise <= start - bleed && (l == 0 || noise >= prevEnd + bleed))
-            {
-                type = TerrainTypes.GRASS;
-                height = bottomHeight;
-                break;
-            }
-            if(noise >= end + bleed && (l == levelCount-1 || noise <= nextStart))
-            {
-                type = TerrainTypes.GRASS;                
-                height = topHeight;
-                break;
-            }
-        }
-
-        height += terrainHeight;
-        height += (int)math.lerp(0, 6, noise);
-
-        return new Topology{
-            height = height,
-            type = type
-        };
-    }
-
     Topology GetCellHeight(CellData cell)
     {
         int height = terrainHeight;
         TerrainTypes type = 0;
 
-        float value = cell.currentCellValue;
+        float cellValue = cell.currentCellValue;
         float adjacentValue = cell.adjacentCellValue;
 
         float increment = 1.0f / levelCount;
 
-        float cellHeight = 0;
-        float adjacentHeight = 0;
-
-        //  Get height of cells
-        for(int l = 1; l < levelCount; l++)
-        {
-            float nextStart = (increment * (l+1));
-            float prevEnd = (increment * (l-1));
-
-            //  Adjacent cell height
-            if((adjacentValue >= prevEnd || l == 0) && (adjacentValue <= nextStart || l == levelCount-1))
-            {
-                adjacentHeight = levelHeight * l;
-            }
-            //  This cell height
-            if((value >= prevEnd || l == 0) && (value <= nextStart || l == levelCount-1))
-            {
-                cellHeight = levelHeight * l;
-            }
-        }
+        float cellHeight = math.lerp(0, levelCount, cellValue) * levelHeight;
+        float adjacentHeight = math.lerp(0, levelCount, adjacentValue) * levelHeight;
         
-        float depth = cliffDepth *2;
-
         //  Close to the edge between two cells of different heights = cliff
-        if(cell.distance2Edge < depth*2 && cellHeight != adjacentHeight)
+        if(cell.distance2Edge < cliffDepth*2 && cellHeight != adjacentHeight)
         {
             type = TerrainTypes.CLIFF;            
         
             //  Closer to the edge between cells, interpolate
             //  between cell heigts for smooth transition
-            if(cell.distance2Edge < depth) 
+            if(cell.distance2Edge < cliffDepth) 
             {
                 float halfway = (cellHeight + adjacentHeight) / 2;
-                float interpolator = Mathf.InverseLerp(0, depth, cell.distance2Edge);
+                float interpolator = Mathf.InverseLerp(0, cliffDepth, cell.distance2Edge);
 
                 //  Interpolate towards midpoint using distance from midpoint
                 height += (int)math.lerp(halfway, cellHeight, interpolator);
