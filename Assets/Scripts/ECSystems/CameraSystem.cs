291defd1c40257fa05734d29b8b7f11dbf61162c
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-02-17 06:52:10 +0000

    increased zoom out max distance

--- Assets/Scripts/ECSystems/CameraSystem.cs
+++ Assets/Scripts/ECSystems/CameraSystem.cs
@@ -59,7 +59,7 @@ public class CameraSystem : ComponentSystem
         //  Clamp zoom 
         float3 withZoom = currentOffset + zoomOffset;
         float magnitude = math.sqrt(math.pow(withZoom.x, 2) + math.pow(withZoom.y, 2) + math.pow(withZoom.z, 2));
-        if(magnitude > 10 && magnitude < 40)
+        if(magnitude > 10 && magnitude < 150)
         {
             //  x & z smoothed for zoom because y is smoothed for everything later
             //  This prevents jumpy camera movement when zooming
