2d2652a5a9020e34bf8c10c8bb5688ccc21e37d6
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-02-17 17:52:25 +0000

    constant

--- Assets/Scripts/TerrainSettings.cs
+++ Assets/Scripts/TerrainSettings.cs
@@ -6,16 +6,16 @@ public enum TerrainTypes { DIRT, GRASS, CLIFF }
 
 public static class TerrainSettings
 {
-	public static int mapSquareWidth = 12;
-	public static int viewDistance = 8;
+	public const int mapSquareWidth = 12;
+	public const int viewDistance = 8;
 
 	//	Must always be at >= squareWidth
-	public static int terrainHeight = 16;
-	public static int seed = 5678;
+	public const int terrainHeight = 16;
+	public const int seed = 5678;
 
-	public static float cellFrequency = 0.02f;
-	public static float cellEdgeSmoothing = 10.0f;
-	public static float cellularJitter = 0.15f;
+	public const float cellFrequency = 0.02f;
+	public const float cellEdgeSmoothing = 10.0f;
+	public const float cellularJitter = 0.15f;
 
 	public static int BiomeIndex(float noise)
 	{
