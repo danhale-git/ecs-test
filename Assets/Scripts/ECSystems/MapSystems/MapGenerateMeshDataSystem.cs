2df54133ac7cc240c7c6d304066c53a0651492fc
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-03-19 18:31:59 +0000

    Generate Mesh Data system added

---
+++ Assets/Scripts/ECSystems/MapSystems/MapGenerateMeshDataSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Transforms;
+using MyComponents;
+
+[UpdateAfter(typeof(MapGenerateMeshDataSystem))]
+public class MapGenerateMeshDataBufferSystem : EntityCommandBufferSystem { }
+
+public class MapGenerateMeshDataSystem : JobComponentSystem
+{
+    EntityManager entityManager;
+    MapGenerateMeshDataBufferSystem bufferSystem;
+    
+    protected override void OnCreateManager()
+    {
+        entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        bufferSystem = World.Active.GetOrCreateManager<MapGenerateMeshDataBufferSystem>();
+
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        return inputDependencies;
+    }
+
+    [RequireComponentTag(typeof(Faces))]
+    public struct WorleyJob : IJobProcessComponentDataWithEntity<MapSquare>
+    {
+        public void Execute(Entity mapSquareEntity, int jobIndex, ref MapSquare mapSquare)
+        {
+
+        }
+    }
+}
